3e572af298e02195787a2311734b1f2f874183d0
E. Filweb <138451037+eugenefilweb@users.noreply.github.com>
2024-01-16 05:29:36 +0800

    rer

---
+++ widgets/views/report-template/rer1.php
@@ -0,0 +1,133 @@
+<main style="padding: 30px 20px 20px 20px;">
+<div style="display: flex; justify-content: space-between;">
+    <span>General Form No. 2</span>
+    <span>Appendex 46</span>
+</div>
+<div>Revised March 2016</div>
+
+<div style="display: flex; justify-content: center;">
+    <h3>REIMBURSEMENT RECEIPT</h3>
+</div>
+
+<table style="border: 1px solid #000; border-collapse: collapse; width: 100%;">
+    <tbody>
+        <tr>
+            <td style="border: 1px solid #000; padding: 20px 5px 10px 5px;">
+                <div style="display: flex;">
+                    <span>Entity Name:</span>
+                    <div style="border-bottom: 1px solid #000; flex-grow: 1;">
+                    </div>
+                </div>
+                <div style="display: flex;">
+                    <span>Date:</span>
+                    <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                </div>
+            </td>
+            <td style="border: 1px solid #000; padding: 20px 5px 10px 5px;">
+                <div style="display: flex;">
+                    <span>Fund Cluster:</span>
+                    <span style="border-bottom: 1px solid #000; flex-grow: 1;"></span>
+                </div>
+                <div style="display: flex;">
+                    <span>RER No:</span>
+                    <span style="border-bottom: 1px solid #000; flex-grow: 1;"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 60px; padding-right: 10px;">
+                <div style="display: flex; justify-content: flex-end;">
+                     <div>RECEIVED from</div>
+                     <span style="width: 400px; border-bottom: 1px solid #000;"></span>
+                 </div>
+                <div style="position: absolute; right: 250px; ">(Name)</div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 60px; padding-right: 10px; padding-left: 10px;">
+                <div style="display: flex;">
+                    <span style="width: 400px; flex-grow: 1; border-bottom: 1px solid #000;"></span>
+                    <span>the amount</span>
+                </div>
+                <div style="position: absolute; left: 200px;">(Official Designation)</div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 60px; padding-right: 10px; padding-left: 10px;">
+                <div style="display: flex;">
+                    <div style="display: flex; flex-direction: column; flex-grow: 1;">
+                        <div style="display: flex;">
+                            <span>of</span>
+                            <div style="border-bottom: 1px solid #000; width: 100%;"></div>
+                        </div>
+                        <div style="display: flex; margin-left: 100px;">
+                            (In Words)
+                        </div>
+                    </div>
+                    <div style="display: flex; flex-direction: column; width: calc((1/4) * 100%);">
+                        <div style="display: flex;">
+                            <span>(P</span>
+                            <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                            <span>)</span>
+                        </div>
+                        <div style="display: flex; justify-content: center;">(In Figures)</div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 60px; padding-right: 10px; padding-left: 10px;">
+                <div style="display: flex; flex-direction: column;">
+                    <div style="display: flex;">
+                        <span>in payment for</span>
+                        <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                    </div>
+                    <div style="display: flex; margin-left: 30%;">(Payments for subsistence, services)</div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 60px; padding-right: 10px; padding-left: 10px;">
+                <div style="border-bottom: 1px solid #000;"></div>
+                <div style="text-align: center;">rental or transport should show inclusive dates</div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 60px; padding-right: 10px; padding-left: 10px;">
+                <div style="border-bottom: 1px solid #000;"></div>
+                <div style="text-align: center">purpose, distance, inclusive points of travel, etc.</div>
+            </td>
+        </tr>
+
+        <tr style="border: 1px solid #000; height: 10px;"></tr>
+        <tr style="border: 1px solid #000;">
+            <td colspan="2" style="padding: 10px;">
+                <h4 style="text-align: center;">PAYEE</h4>
+                <div style="display: flex; margin-bottom: 10px;">
+                    <span>Name/Signature</span>
+                    <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                </div>
+                <div style="display: flex; margin-bottom: 10px;">
+                    <span>Address</span>
+                    <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                </div>
+            </td>
+        </tr>
+        <tr style="border: 1px solid #000; height: 10px;"></tr>
+        <tr style="border: 1px solid #000;">
+            <td colspan="2" style="padding: 10px;">
+                <h4 style="text-align: center;">WITNESS</h4>
+                <div style="display: flex; margin-bottom: 10px;">
+                    <span>Name/Signature</span>
+                    <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                </div>
+                <div style="display: flex; margin-bottom: 10px;">
+                    <span>Address</span>
+                    <div style="border-bottom: 1px solid #000; flex-grow: 1;"></div>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</main>
+
