1617f8c2e00a5c60e0c2a326dac12f35a1ccd603
eugene <eugene.filweb@gmail.com>
2024-01-18 17:03:54 +0800

    disbursement voucher form

---
+++ disbursement_voucher/disbursement_voucher.php
@@ -0,0 +1,147 @@
+
+
+
+<table style="border-collapse: collapse; width: 100%; height: 371px; font-family: 'times new roman', times, serif;" border="1">
+    <tbody>
+
+    <tr style="height: 66px;">
+        <td style="width: 100%;" colspan="6">
+            <div style="display: flex; justify-content: center; gap: 10px;">
+                <div style="box-sizing: border-box;">
+                    <img alt="logo" src="" style="box-sizing: border-box; width: 70px; height: 70px; vertical-align: middle; border-style: none; display: block; margin: 0 auto 0 auto;"/>
+                </div>
+                <div style="text-align: center;">
+                    <div>Republic of the Philippines</div>
+                    <div style="font-weight: bold;">MUNICIPALITY OF GENERAL NAKAR</div>
+                    <div>Province of Quezon</div>
+                </div>
+            </div>
+        </td>
+    </tr>
+
+    <tr style="height: 33px; position: relative;">
+        <td colspan="4" style="text-align: center; font-weight: bold; font-size: 20pt;">DISBURSEMENT VOUCHER</td>
+        <td colspan="2" style="position: absolute; bottom: 0; border-style: none; margin-left: 5px;">No.</td>
+    </tr>
+
+<!--    <tr style="height: 33px;">-->
+<!--        <td style="padding-left: 5px;">Mode of <br> Payment</td>-->
+<!--        <td colspan="5" >-->
+<!--            <div style="display: flex; justify-content: center; gap: 30px;">-->
+<!--                <div><span style="padding: 0px 15px 0px 15px; border: 1px solid #000;"></span><span style="margin-left: 10px;">Check</span></div>-->
+<!--                <div><span style="padding: 0px 15px 0px 15px; border: 1px solid #000;"></span><span style="margin-left: 10px;">Cash</span></div>-->
+<!--                <div><span style="padding: 0px 15px 0px 15px; border: 1px solid #000;"></span><span style="margin-left: 10px;">Others</span></div>-->
+<!--            </div>-->
+<!--        </td>-->
+<!--    </tr>-->
+
+<!--    <tr style="height: 33px;">-->
+<!--        <td style="padding-left: 5px; vertical-align: bottom;">Payee</td>-->
+<!--        <td colspan="3"></td>-->
+<!--        <td style="padding-left: 5px;">TIN/Employee <br> No.</td>-->
+<!--        <td style="vertical-align: text-top; text-align: center;">Obligation Request No.</td>-->
+<!--    </tr>-->
+<!---->
+<!--    <!--    <tr style="height: 20px;">-->-->
+<!--    <!--        <td style="padding-left: 5px;" rowspan="2">Address</td>-->-->
+<!--    <!--        <td colspan="3" rowspan="2"></td>-->-->
+<!--    <!--        <td colspan=""></td>-->-->
+<!--    <!--        <td colspan="2"></td>-->-->
+<!--    <!--    </tr>-->-->
+<!---->
+<!--    <tr style="height: 22px">-->
+<!--        <td style="padding-left: 5px;" rowspan="2">Address</td>-->
+<!--        <td colspan="3" rowspan="2"></td>-->
+<!--        <td colspan="2" style="text-align: center;">Responsibility Center</td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 33px;">-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top;">Office/Unit/Project</td>-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top;">Code</td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 22px;">-->
+<!--        <td colspan="5" rowspan="2"></td>-->
+<!--        <td style="text-align: center;">AMOUNT</td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 360px;">-->
+<!--        <td></td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 88px;">-->
+<!--        <td colspan="3" style="position: relative;">-->
+<!--            <div style="position: absolute; top: 0; left: 0;">-->
+<!--                <span style="padding: 0 5px 0 5px; border: 2px solid #000; margin-right: 15px; display: inline-block;">A</span>-->
+<!--                <span>Certified</span>-->
+<!--            </div>-->
+<!--            <div>-->
+<!--                <div style="margin-left: 5px; font-size: 11pt;"><span style="margin-left: 20px; padding: 6px 8px 6px 8px; border: 1px solid #000; display: inline-block;"></span> <span>Alloted obligated for the purpose as indicated <br> above</span></div>-->
+<!--            </div>-->
+<!--            <div style="position: absolute; bottom: 0;">-->
+<!--                <div style="margin-left: 5px; font-size: 11pt;"><span style="margin-left: 20px; padding: 6px 8px 6px 8px; border: 1px solid #000; display: inline-block;"></span> <span>Supporting documents complete</span></div>-->
+<!--            </div>-->
+<!--        </td>-->
+<!--        <td colspan="3" style="position: relative;">-->
+<!--            <div style="position: absolute; top: 0; left: 0;">-->
+<!--                <span style="padding: 0 5px 0 5px; border: 2px solid #000; margin-right: 15px; display: inline-block;">B</span>-->
+<!--                <span>Certified</span>-->
+<!--            </div>-->
+<!--            <div style="text-align: center;">-->
+<!--                <span>Funds Available</span>-->
+<!--            </div>-->
+<!--        </td>-->
+<!--    </tr>-->
+
+<!--    <tr style="height: 22px;">-->
+<!--        <td colspan="3"></td>-->
+<!--        <td colspan="3"></td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 33px;">-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top; ">Signature:</td>-->
+<!--        <td colspan="2"></td>-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top; ">Signature:</td>-->
+<!--        <td colspan="2"></td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 33px;">-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top;">Printed <br> Name</td>-->
+<!--        <td style="vertical-align: bottom; text-align: center; padding-left: 5px; font-weight: bold; width: calc((3/10) * 100%);">DIANA F. CUERDO, CPA</td>-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top; padding-left: 5px; ">Date</td>-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top;">Printed <br> Name</td>-->
+<!--        <td colspan="2" style="vertical-align: bottom; text-align: center; padding-left: 5px; font-weight: bold;">JOCELYN B. JOVIDA</td>-->
+<!--    </tr>-->
+<!---->
+<!--    <tr style="height: 33px;">-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top;">Position</td>-->
+<!--        <td style="font-style: italic; text-align: center; vertical-align: text-top; font-size: 10pt; width: calc((3/10) * 100%); border-right: 0px solid #000;">Municipal Accountant</td>-->
+<!--        <td style="border-left: 0px solid #000;"></td>-->
+<!--        <td style="padding-left: 5px; vertical-align: text-top;">Position</td>-->
+<!--        <td colspan="2" style="font-size: 9pt; padding-left: 5px; font-style: italic;">Acting Municipal Treasurer <br> Head, Treasury Unit/Authorized Representative</td>-->
+<!--    </tr>-->
+
+<!--    <tr style="height: 22px;">-->
+<!--        <td colspan="3" style="position: relative;">-->
+<!--            <div style="box-sizing: border-box; width: 30px; border-right: 1px solid #000; position: absolute; top: 0; bottom: 0; height: 100%; display: flex; justify-content: center; align-items: center;">-->
+<!--                <span>C</span>-->
+<!--            </div>-->
+<!--            <span style="margin-left: 30px; display: inline-block; padding-left: 5px;">Approved for Payment</span>-->
+<!--        </td>-->
+<!--        <td colspan="3" style="position: relative;">-->
+<!--            <div style="box-sizing: border-box; width: 30px; border-right: 1px solid #000; position: absolute; top: 0; bottom: 0; height: 100%; display: flex; justify-content: center; align-items: center;">-->
+<!--                <span>D</span>-->
+<!--            </div>-->
+<!--            <span style="margin-left: 30px; display: inline-block; padding-left: 5px;">Received Payment</span>-->
+<!--        </td>-->
+<!--    </tr>-->
+
+<!--    <tr style="height: 22px;">-->
+<!--        <td>Signature:</td>-->
+<!--        <td colspan="2"></td>-->
+<!--        <td coslpan="4"></td>-->
+<!--    </tr>-->
+
+
+    </tbody>
+</table>
---
+++ disbursement_voucher/disbursement_voucher2.php
@@ -0,0 +1,46 @@
+<table border="1" style="border-collapse: collapse; width: 100%;">
+
+    <tbody>
+        <tr style="height: 66px;">
+            <td style="width: 100%;" colspan="6">
+                <div style="display: flex; justify-content: center; gap: 10px;">
+                    <div style="box-sizing: border-box;">
+                        <img alt="logo" src="" style="box-sizing: border-box; width: 70px; height: 70px; vertical-align: middle; border-style: none; display: block; margin: 0 auto 0 auto;"/>
+                    </div>
+                    <div style="text-align: center;">
+                        <div>Republic of the Philippines</div>
+                        <div style="font-weight: bold;">MUNICIPALITY OF GENERAL NAKAR</div>
+                        <div>Province of Quezon</div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+
+        <tr style="height: 33px;">
+            <td colspan="4" style="width: calc((2/3) * 100%); text-align: center; font-weight: bold; font-size: 20pt;">DISBURSEMENT VOUCHER</td>
+            <td colspan="2" style="width: calc((1/3) * 100%); vertical-align: bottom; padding-left: 5px;">No.</td>
+        </tr>
+
+<!--        <tr style="height: 33px;">-->
+<!--            <td style="width: calc((1/3) * 100%);padding-left: 5px;">Mode of <br> Payment</td>-->
+<!--            <td colspan="5" >-->
+<!--                <div style="width: calc((5/6) * 100%); display: flex; justify-content: center; gap: 30px;">-->
+<!--                    <div><span style="padding: 0px 15px 0px 15px; border: 1px solid #000;"></span><span style="margin-left: 10px;">Check</span></div>-->
+<!--                    <div><span style="padding: 0px 15px 0px 15px; border: 1px solid #000;"></span><span style="margin-left: 10px;">Cash</span></div>-->
+<!--                    <div><span style="padding: 0px 15px 0px 15px; border: 1px solid #000;"></span><span style="margin-left: 10px;">Others</span></div>-->
+<!--                </div>-->
+<!--            </td>-->
+<!--        </tr>-->
+
+    <tr style="height: 33px;">
+        <td style="width: calc((2/3) * 100%);"></td>
+        <td style="width: calc((2/3) * 100%);"></td>
+        <td style="width: calc((2/3) * 100%);"></td>
+        <td style="width: calc((2/3) * 100%);"></td>
+        <td style="width: calc((2/3) * 100%);"></td>
+        <td style="width: calc((2/3) * 100%);"></td>
+    </tr>
+
+    </tbody>
+
+</table>
